9ade46ff2a3135ebadf80aedc26ea9fd2ebe7800
banana0816 <Zzhu@putnamscience.org>
2025-04-29 14:18:19 -0400

    1

---
+++ __pycache__/one.py
@@ -0,0 +1,24 @@
+class books():
+    def __init__(self):
+        self.name = "xiaohan"
+        self.title = "x"
+        self.price = 0
+        self.book = {}
+
+    def Add(self):
+        self.title = input("Enter the name of the book you add.")
+        self.author = input("Enter the author of the book you add.")
+        self.price = float(input("Enter the listing price of the book you add."))
+
+        print("You have added", self title)
+        print(self.books)
+ 
+def Muins(self):
+    self.title = input("Enter the name of the book you remove   \n")
+    del self.Books[self.title]
+    print(self.Books)
+
+
+    
+      
+ 
