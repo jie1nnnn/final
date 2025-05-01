3ef69efab278c52bfeef2c5f93a9985357abdbfa
banana0816 <Zzhu@putnamscience.org>
2025-05-01 09:09:42 -0400

    Update one.py

--- __pycache__/one.py
+++ __pycache__/one.py
@@ -6,19 +6,44 @@ class books():
         self.book = {}
 
     def Add(self):
-        self.title = input("Enter the name of the book you add.")
-        self.author = input("Enter the author of the book you add.")
-        self.price = float(input("Enter the listing price of the book you add."))
+        self.title = input("Enter the name of the book you add      \n")
+        self.author = input("Enter the author of the book you add       \n")
+        self.price = float(input("Enter the listing price of the book you add      \n"))
 
         print("You have added", self title)
         print(self.books)
  
 def Muins(self):
     self.title = input("Enter the name of the book you remove   \n")
-    del self.Books[self.title]
-    print(self.Books)
+    try:
+        str(self.title)
+        if self.title in self.Book:
+          del self.Books[self.title]
+          print(self.Books)
+
+    except:
+        print("Plases enter a book title")
 
 
-    
-      
- 
+
+def showbook(self):
+    for x in self.Books:
+        print(x)
+        print(self.Books[x]["author"]) 
+        print(self.Books[x]["price"]) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
