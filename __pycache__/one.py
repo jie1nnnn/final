286cce2853016f142d3c4cd5fd9c3e4d563194a3
banana0816 <Zzhu@putnamscience.org>
2025-05-08 14:47:53 -0400

    Update one.py

--- __pycache__/one.py
+++ __pycache__/one.py
@@ -1,20 +1,41 @@
-from one import books
-a = books()
-v = ""
-print(a.title)
+class books():
+    def __init__(self):
+        self.name = "xiaohan"
+        self.title = "x"
+        self.price = 0
+        self.book = {}
 
-while v !="no":
+    def Add(self):
+        self.title = input("Enter the name of the book you add.")
+        self.author = input("Enter the author of the book you add.")
+        self.price = float(input("Enter the listing price of the book you add."))
 
-    v = input("would you like to add a book(yes or no)?")
-    if v == "yes":
-        c = a.Add()
+       
+        print("You have added", self title)
+        print(self.books)
+ 
+def Muins(self):
+    self.title = input("Enter the name of the book you remove   \n")
+    try:
+        str(self.title)
+        if self.title in self.Book:
+          del self.Books[self.title]
+          print(self.Books)
 
-    elif v == "no":
-        c = a.minus()
-    elif v == "y":
-        e = a.showbook()
-    elif
-    else:
-        print("please enter 'yes' or 'no'.")
+    except:
+        print("Plases enter a book title")
 
-     
+
+def showbook(self):
+    for x in self.Books:
+        print(x)
+        print(self.Books[x]['Author'])  
+        print(self.Books[x]["price"])  
+
+def save(self):
+    with open("append_file.txt", "a") as file:
+       file.write(str self.Books)
+       print(f" (self.title) saved to (name)")
+
+def 
+
